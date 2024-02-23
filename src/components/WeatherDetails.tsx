f8dccf0b33c3d8265112525224c3496de93b560f
David Saunders <djsaun+github@gmail.com>
2024-02-23 14:25:01 -0800

    Add iconography to WeatherDetails component

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,6 +1,11 @@
 import {FC} from 'react';
-import styles from './WeatherDetails.module.css'
+import styles from './WeatherDetails.module.css';
 import {WeatherData} from "../hooks/useWeatherData";
+import wind from '../assets/wind.svg';
+import temperature from '../assets/temperature.svg';
+import raincloud from '../assets/raincloud.svg';
+import raindrop from '../assets/droplet.svg';
+
 
 interface WeatherDetailsProps {
   weatherData: WeatherData;
@@ -9,6 +14,7 @@ export const WeatherDetails: FC<WeatherDetailsProps> = ({weatherData}) => {
   const {name, country, region, localtime} = weatherData.location
   const {text, icon} = weatherData.current.condition
   const {temp_f, feelslike_f, wind_mph, wind_dir, humidity, precip_in} = weatherData.current
+
   return <div className={styles.weatherDetailsContainer}>
     <h1>{name}, {country === 'United States of America' ? region : country}</h1>
 
@@ -20,20 +26,32 @@ export const WeatherDetails: FC<WeatherDetailsProps> = ({weatherData}) => {
 
     <div className={styles.moreDetails}>
       <div>
-        <p>Feels like:</p>
-        <p>{`${feelslike_f}° F`}</p>
+        <img src={temperature} alt={''} />
+        <div>
+          <p>Feels like:</p>
+          <p>{`${feelslike_f}° F`}</p>
+        </div>
       </div>
       <div>
-        <p>Wind:</p>
-        <p>{`${wind_mph} mph`} {wind_dir}</p>
+        <img src={wind} alt={''} />
+        <div>
+          <p>Wind:</p>
+          <p>{`${wind_mph} mph`} {wind_dir}</p>
+        </div>
       </div>
       <div>
-        <p>Humidity:</p>
-        <p>{humidity}%</p>
+        <img src={raindrop} alt={''} />
+        <div>
+          <p>Humidity:</p>
+          <p>{humidity}%</p>
+        </div>
       </div>
       <div>
-        <p>Precipitation:</p>
-        <p>{`${precip_in} in`}</p>
+        <img src={raincloud} alt={''} />
+        <div>
+          <p>Precipitation:</p>
+          <p>{`${precip_in} in`}</p>
+        </div>
       </div>
     </div>
 
